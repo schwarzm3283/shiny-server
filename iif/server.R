0470d90d34d1d145160ecaebf072a05955dfa792
schwarzm3283 <schwarz.mike@gmail.com>
2017-04-20 00:21:02 +0000

    first commit

---
+++ iif/server.R
@@ -0,0 +1,46 @@
+# Michael Schwarz
+# Michael 08 2017
+
+# This is the server portion of a shiny app that returns IIF lab stat tdata
+
+source("utils.R")
+#import lookup tables data
+
+library(shiny)
+library(shinyjs)
+library(stringr)
+library(blscrapeR)
+library(plotly)
+library(lazyeval)
+library(shinysky)
+library(RPostgreSQL)
+library(DT)
+library(htmltools)
+library(pool)
+library(DBI)
+library(tidyverse)
+
+
+#------------------db connection------------------  
+source("database_config.R",  local = TRUE)$value
+
+shinyServer(function(input, output, session) {
+  busyIndicator("Loading Data",wait = 0)
+  
+
+#---------------Create sidebar menu
+  
+  output$menu <- renderMenu({
+    sidebarMenu(
+      menuItem("About", tabName = "info", icon = icon("info")),
+      menuItem("Industry", tabName = "industry",  icon = icon("database")),
+      menuItem("Case and Demographics", tabName = "caseanddemo", icon = icon("database")),
+      menuItem("Fatal", tabName = "fatal", icon = icon("database"))
+    )
+  })
+  isolate({updateTabItems(session, "tabs", "info")})
+  #include server source files
+  source(file.path("server", "industry.R"),  local = TRUE)$value
+ 
+ 
+  })
